f3afb2e7074b0b5d0b59daa4c7084079d254ff1b
Chris Russell <48478211+chrisrivendi@users.noreply.github.com>
2019-08-30 14:14:29 +0100

    RabbitConfig for ImageApi

---
+++ services/ImageApi/Controllers/ImageRetrieveController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Dto;
+using Core.Events;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace ImageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageRetrieveController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public ImageRetrieveController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        // POST: api/ImageRetrieve
+        [HttpPost]
+        public void Post([FromBody] ImageRetrieve image)
+        {
+            _logger.Information(image.RedditId);
+
+            ImageFound i = new ImageFound()
+            {
+                RedditId = image.RedditId,
+                Subreddit = image.Subreddit,
+                Author = image.Author,
+                Url = image.Url,
+                CreatedUtc = image.CreatedUtc
+            };
+        }
+    }
+}
--- services/ImageApi/Startup.cs
+++ services/ImageApi/Startup.cs
@@ -2,22 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ImageApi.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Serilog;
 
 namespace ImageApi
 {
     public class Startup
     {
+        private AppSettings _appSettings;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _appSettings = new AppSettings();
+            Configuration.Bind("Settings", _appSettings);
+
+            ConfigureLogger();
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +33,8 @@ namespace ImageApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<ILogger>(Log.Logger);
+            services.AddSingleton<AppSettings>(_appSettings);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +52,17 @@ namespace ImageApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private ILogger ConfigureLogger()
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo
+                .Console();
+
+            Log.Logger = logger.CreateLogger();
+
+            return Log.Logger;
         }
     }
 }
